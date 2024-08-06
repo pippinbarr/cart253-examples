3984bc24927d21fb156118537ad5567bc555bb0a
Pippin Barr <pippin.barr@gmail.com>
2024-08-06 12:03:08 -0400

    Started Drawing module

---
+++ topics/instructions/drawing/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Drawing Module
+ * Pippin Barr
+ * 
+ * Practicing and learning drawing functions in p5
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
