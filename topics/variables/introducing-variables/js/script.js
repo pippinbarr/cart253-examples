458ddeba4a191acede42783f879b192d7e2f42c7
Pippin Barr <pippin.barr@gmail.com>
2024-08-09 10:34:46 -0400

    Started the Introducing Variables project

---
+++ topics/variables/introducing-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing variables
+ * Pippin Barr
+ * 
+ * Learning what a variable is and does
+ */
+
+"use strict";
+
+/**
+ * Create a canvas
+*/
+function setup() {
+    
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
