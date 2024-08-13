31a5e6bb8fbb8227ce39115b36cc421d6b15cf0e
Pippin Barr <pippin.barr@gmail.com>
2024-08-13 11:45:14 -0400

    Created a time example

--- topics/events/time/js/script.js
+++ topics/events/time/js/script.js
@@ -1,24 +1,20 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+const ball = {
+    x: 0,
+    y: 200,
+    size: 50
+};
 
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+    createCanvas(400, 400);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(0);
 
+    ball.x += 1;
+
+    push();
+    noStroke();
+    ellipse(ball.x, ball.y, ball.size);
+    pop();
 }
