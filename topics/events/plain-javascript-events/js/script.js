34e7389c8d6260f5be6228ca4d06ce91f3e919df
Pippin Barr <pippin.barr@gmail.com>
2024-08-13 14:25:18 -0400

    Started the Plain JavaScript Events project

---
+++ topics/events/plain-javascript-events/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Plain JavaScript Events
+ * Pippin Barr
+ * 
+ * Experimenting with event handling in Plain JavaScript
+ */
+
+"use strict";
+
+/**
+ * 
+*/
+function setup() {
+
+}
+
+
+/**
+ * 
+*/
+function draw() {
+
+}
