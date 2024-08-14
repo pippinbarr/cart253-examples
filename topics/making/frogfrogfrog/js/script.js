c61137efe24cc79ceff5f6d133c01f18d88c79aa
Pippin Barr <pippin.barr@gmail.com>
2024-08-14 13:38:19 -0400

    Started the Frogfrogfrog project

---
+++ topics/making/frogfrogfrog/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Frogfrogfrog
+ * Pippin Barr
+ * 
+ * A game of catching flies with your frog-tongue
+ * 
+ * Instructions:
+ * - Move the frog with your mouse
+ * - Click to launch the tongue
+ * - Catch flies
+ * 
+ * Made with p5
+ * https://p5js.org/
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
