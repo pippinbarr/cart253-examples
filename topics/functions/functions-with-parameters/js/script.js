938c445ed99a07b26ee3683cfbe6610dacf67dba
Pippin Barr <pippin.barr@gmail.com>
2024-09-09 13:44:19 -0400

    Started Functions with parameters

---
+++ topics/functions/functions-with-parameters/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Functions with parameters
+ * Pippin Barr
+ * 
+ * Experimenting with parameters
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
