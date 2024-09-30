39abde55dda8a13856aa04ddb21266f45e2eac48
Pippin Barr <pippin.barr@gmail.com>
2024-09-30 12:40:09 -0400

    Finished Flies flies flies! project

--- topics/arrays/flies-flies-flies/js/script.js
+++ topics/arrays/flies-flies-flies/js/script.js
@@ -9,29 +9,27 @@
 
 // Our array of flies (specifically "fly data" really)
 // Each fly has a position and a size
-let flies = [
-    {
-        x: 100,
-        y: 125,
-        size: 10
-    },
-    {
-        x: 160,
-        y: 170,
-        size: 14
-    },
-    {
-        x: 180,
-        y: 50,
-        size: 5
-    }
-];
+let flies = [];
 
 /**
  * Creates the canvas
  */
 function setup() {
     createCanvas(300, 300);
+}
+
+/**
+ * Returns a random fly!
+ */
+function createFly() {
+    // Generate a random fly
+    let fly = {
+        x: random(0, width),
+        y: random(0, height),
+        size: random(50, 60),
+        buzziness: random(2, 8)
+    };
+    return fly;
 }
 
 /**
@@ -65,4 +63,31 @@ function drawFly(fly) {
     fill(0);
     ellipse(fly.x, fly.y, fly.size);
     pop();
+}
+
+/**
+ * Adds a random fly on a key press
+ */
+function keyPressed() {
+    // Create a new fly
+    const newFly = createFly();
+    // Add it to the array
+    flies.push(newFly);
+}
+
+/**
+ * Remove a fly from the array if clicked
+ */
+function mousePressed() {
+    // Check all the flies...
+    for (let fly of flies) {
+        const d = dist(mouseX, mouseY, fly.x, fly.y);
+        if (d < fly.size / 2) {
+            // This fly was clicked!
+            // Get the index of this fly
+            const index = flies.indexOf(fly);
+            // Splice it out of the array
+            flies.splice(index, 1);
+        }
+    }
 }
