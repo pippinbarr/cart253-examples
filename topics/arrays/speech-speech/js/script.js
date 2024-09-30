515c3300f6a5ad16603941dee9992e11a9b0e40a
Pippin Barr <pippin.barr@gmail.com>
2024-09-30 11:51:22 -0400

    Finished Speech! Speech!

--- topics/arrays/speech-speech/js/script.js
+++ topics/arrays/speech-speech/js/script.js
@@ -7,17 +7,45 @@
 
 "use strict";
 
+// The speech itself
+const speech = ["Veni.", "Vidi.", "Vici.", "Sensi malum."];
+// Which sentence in the speech to display
+let speechIndex = 0;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create canvas
 */
 function setup() {
-
+    createCanvas(600, 100);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Display the current line of the speech
 */
 function draw() {
+    background(0);
 
+    // Get the current line of the speech
+    let currentLine = speech[speechIndex];
+
+    // Display the line
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentLine, width / 2, height / 2);
+    pop();
+}
+
+/**
+ * Advances the speech on a mouse click
+ */
+function mousePressed() {
+    // Next line
+    speechIndex = speechIndex + 1;
+    // Handle the end of the speech
+    if (speechIndex >= speech.length) {
+        // Start over
+        speechIndex = 0;
+    }
 }
