8a596a4067b51ffc75e9ef5f3931be6d3a385004
Pippin Barr <pippin.barr@gmail.com>
2024-10-02 12:06:26 -0400

    Finished the Gradient project

--- topics/loops/gradient/js/script.js
+++ topics/loops/gradient/js/script.js
@@ -1,24 +1,37 @@
 /**
- * Title of Project
- * Author Name
+ * Gradient
+ * Pippin Barr
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Draws a gradient out of lines
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a canvas
 */
 function setup() {
-
+    createCanvas(600, 300);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a gradient
 */
 function draw() {
+    background(0);
 
+    let y = 0;
+
+    randomSeed(0);
+    for (let x = 0; x <= width; x += 1) {
+        const shade = map(x, 0, width, 0, 255);
+
+        push();
+        stroke(shade);
+        line(x, y, x, height);
+        pop();
+
+        y += random(0, 2);
+    }
 }
