dd4bcf3cec056ef764783b91e9a25fd0b8f18886
Pippin Barr <pippin.barr@gmail.com>
2024-10-30 17:18:29 -0400

    Completed the Tarot project

--- topics/data/tarot/js/script.js
+++ topics/data/tarot/js/script.js
@@ -7,17 +7,44 @@
 
 "use strict";
 
-/**
- * tbd.
-*/
-function setup() {
+// Our tarot data
+let tarot = undefined;
 
+// Our fortune
+let fortune = "Click to show a fortune.";
+
+/**
+ * Load tarot data
+ */
+function preload() {
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
 }
 
+/**
+ * Create a canvas
+*/
+function setup() {
+    createCanvas(800, 400);
+}
 
 /**
- * tbd.
+ * Display tarot
 */
 function draw() {
+    background(0);
 
+    // Display the information
+    push();
+    textSize(16);
+    fill("yellow");
+    textAlign(CENTER, CENTER);
+    text(fortune, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    // Choose a random card
+    const card = random(tarot.tarot_interpretations);
+    // Choose a random fortune
+    fortune = random(card.fortune_telling);
 }
